5ac203d16a7d4c52533eebd462dc84f84e535476
nabilazil <nabilazil2011@gmail.com>
2026-08-12 21:39:55 +0100

    Update LNK-Analyzer.ps1

--- Tools/LNK-Analyzer.ps1
+++ Tools/LNK-Analyzer.ps1
@@ -32,5 +32,5 @@ powershell
 1
    .\Analyze-LNK.ps1 -LnkPath "C:\Path\To\Suspicious\file.lnk"
 
-
+<img width="1489" height="659" alt="Image" src="https://github.com/user-attachments/assets/917598ef-5828-42bd-bedc-aa2b926faf14" />
    
